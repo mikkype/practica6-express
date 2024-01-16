d78ad039c3efd6025dfa84e9dd4c7c0f0ae0364f
mikkype <miguelcaceda@gmail.com>
2024-01-16 23:18:04 +0100

    16 eneo casa

---
+++ src/config.db.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const mysql = require('mysql2');
+let connection;
+try {
+ connection = mysql.createConnection({
+ host: process.env.DB_HOST,
+ user: process.env.DB_USER,
+ password: process.env.DB_PASSWORD,
+ database: process.env.DB_NAME,
+ port: process.env.DBPORT
+ });
+} catch (error) {
+ console.log("Error al conectar con la base de datos");
+}
+module.exports = {connection};
