72cdd7fd9832e232aa132a8c4bc63c731d3bd9ea
mikkype <miguelcaceda@gmail.com>
2024-01-17 18:42:03 +0100

    db

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM node
-WORKDIR /app
-COPY ./src /app
-RUN npm install
-RUN npm install mysql2@3.0.0
-
-CMD [ "npm" , "start" ]
--- src/config.db.js
+++ src/config.db.js
@@ -8,8 +8,7 @@ try {
  user: process.env.DB_USER,
  password: process.env.DB_PASSWORD,
  database: process.env.DB_NAME,
- port: process.env.DB_PORT
- });
+ port: process.env.DB_POR });
 } catch (error) {
  console.log("Error al conectar con la base de datos");
 }
