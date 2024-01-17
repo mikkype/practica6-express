7684a5640d172561731b8ef4209e1a50113c2cc9
mikkype <miguelcaceda@gmail.com>
2024-01-17 21:30:45 +0100

    definiendo

--- src/config.db.js
+++ src/config.db.js
@@ -5,10 +5,11 @@ let connection;
 try {
  connection = mysql.createConnection({
  host: process.env.DB_HOST,
+ database:process.env.DB_NAME,
  user: process.env.DB_USER,
  password: process.env.DB_PASSWORD,
- database: process.env.DB_NAME,
- port: process.env.DB_POR });
+ port: process.env.DB_PORT});
+ console.log("conectado a bd");
 } catch (error) {
  console.log("Error al conectar con la base de datos");
 }
