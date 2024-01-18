2e9dcedddab30ef800780c98d3abc1b82094f901
miguel <miguelcaceda@gmail.com>
2024-01-18 13:47:32 +0100

    api rest node express ok

--- src/routes/estudiantes.js
+++ src/routes/estudiantes.js
@@ -4,17 +4,17 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 
-const {connection} = require("../config");
+const { connection } = require("../config");
 
 //GET 
 const getEstudiante = (request, response) => {
     connection.query("SELECT * FROM estudiantes",
-    (error, results) => {
-    if(error)
-    throw error;
-    response.status(200).json(results);
-    });
-   };
+        (error, results) => {
+            if (error)
+                throw error;
+            response.status(200).json(results);
+        });
+};
 
 app.route("/estudiantes").get(getEstudiante);
 
@@ -22,43 +22,43 @@ app.route("/estudiantes").get(getEstudiante);
 const getEstudianteID = (request, response) => {
     const id = request.params.id;
     connection.query("SELECT * FROM estudiantes WHERE id = ?",
-    [id],
-    (error, results) => {
-    if(error)
-    throw error;
-    response.status(201).json({results});
-    });
-   };
+        [id],
+        (error, results) => {
+            if (error)
+                throw error;
+            response.status(200).json({ results });
+        });
+};
 app.route("/estudiantes/:id").get(getEstudianteID);
 
 //POST
 const postEstudiante = (request, response) => {
-    const {nombre,apellido,fecha_nacimiento,direccion,telefono} = request.body;
+    const { nombre, apellido, fecha_nacimiento, direccion, telefono } = request.body;
     connection.query("INSERT INTO estudiantes(nombre,apellido,fecha_nacimiento,direccion,telefono) VALUES (?,?,?,?,?)",
-    
-    [nombre,apellido,fecha_nacimiento,direccion,telefono],
-    (error, results) => {
-    if(error)
-    throw error;
-    response.status(201).json({"Item añadido correctamente": results.affectedRows});
-    });
-   };
-  
-   app.route("/estudiantes").post(postEstudiante)
 
-   //DELETE
-   const delEstudiante = (request, response) => {
+        [nombre, apellido, fecha_nacimiento, direccion, telefono],
+        (error, results) => {
+            if (error)
+                throw error;
+            response.status(201).json({ "Item añadido correctamente": results.affectedRows });
+        });
+};
+
+app.route("/estudiantes").post(postEstudiante)
+
+//DELETE
+const delEstudiante = (request, response) => {
     const id = request.params.id;
     connection.query("Delete from estudiantes where id = ?",
-    [id],
-    (error, results) => {
-    if(error)
-    throw error;
-    response.status(201).json({"Item eliminado":results.affectedRows});
-    });
-   };
-   //ruta
-   app.route("/estudiantes/:id").delete(delEstudiante);
-   
+        [id],
+        (error, results) => {
+            if (error)
+                throw error;
+            response.status(201).json({ "Item eliminado": results.affectedRows });
+        });
+};
+//ruta
+app.route("/estudiantes/:id").delete(delEstudiante);
+
 
 module.exports = app;
--- src/routes/profesores.js
+++ src/routes/profesores.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const {connection} = require("../config");
 
+//GET
 const getProfesor = (request, response) => {
     connection.query("SELECT * FROM profesores",
     (error, results) => {
@@ -16,18 +17,45 @@ const getProfesor = (request, response) => {
    //ruta
 app.route("/profesores").get(getProfesor);
 
+//GET ID
 const getProfesorID = (request, response) => {
     const id = request.params.id;
-    connection.query("SELECT * FROM estudiantes where id = ?",
+    connection.query("SELECT * FROM profesores where id = ?",
     [id],
     (error, results) => {
     if(error)
-    console.error("Error al obtener estudiante por ID:", error);
-                
     throw error;
     response.status(200).json({results});
     });
    };
 app.route("/profesores/:id").get(getProfesorID);
+
+//POST
+const postProfesor= (request, response) => {
+    const { nombre, apellido, especialidad,correo_electronico} = request.body;
+    connection.query("INSERT INTO profesores( nombre, apellido, especialidad,correo_electronico) VALUES (?,?,?,?)",
+    [ nombre, apellido, especialidad,correo_electronico],
+        (error, results) => {
+            if (error)
+                throw error;
+            response.status(201).json({ "Item añadido correctamente": results.affectedRows });
+        });
+};
+app.route("/profesores").post(postProfesor)
+
+//DELETE
+const delProfesor = (request, response) => {
+    const id = request.params.id;
+    connection.query("Delete from profesores where id = ?",
+        [id],
+        (error, results) => {
+            if (error)
+                throw error;
+            response.status(201).json({ "Item eliminado": results.affectedRows });
+        });
+};
+//ruta
+app.route("/profesores/:id").delete(delProfesor);
+
 
 module.exports = app;
